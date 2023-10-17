2eaa818bc5deb05a51e29b27a07c3c58b57180bc
Jonatan Missora <jonatanjmissora@gmail.com>
2023-10-17 14:17:45 -0300

    first commit

---
+++ src/data/tratamientos.js
@@ -0,0 +1,26 @@
+export const TRATAMIENTO_OPTIONS = [
+  "maderoterapia",
+  "lifting",
+  "perfilado",
+  "lifting + higiene",
+  "facial profundo",
+  "facial premium",
+  "uñas",
+  "evaluacion",
+  "depilacion",
+  "otro"
+];
+
+export const DEPILACION_OPTIONS = [
+  "Pierna Entera",
+  "Media Pierna",
+  "Axila",
+  "Brazos",
+  "Bozo",
+  "Rostro Completo",
+  "Pecho",
+  "Espalda Baja",
+  "Espalda",
+  "Cavado Completo con Tiro",
+  "Cavado Completo sin Tiro"
+];
